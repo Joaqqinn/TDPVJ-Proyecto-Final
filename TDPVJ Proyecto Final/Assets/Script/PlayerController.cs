5ccaf9a66ff5d1307e2db236c49f7001947d399f
Joaquín Copello <88283010+Joaqqinn@users.noreply.github.com>
2023-12-07 13:52:52 -0300

    scene test, movement and jump

--- TDPVJ Proyecto Final/Assets/Script/PlayerController.cs
+++ TDPVJ Proyecto Final/Assets/Script/PlayerController.cs
@@ -4,15 +4,59 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    //Player
+    private Rigidbody2D rb;
+    private float movementInputDirection;
+    private bool isFacingRight = true;
+
+    public float movementSpeed = 10.0f;
+    public float jumpForce = 16.0f;
+
+    private void Start()
     {
+        rb = GetComponent<Rigidbody2D>();
+    }
+    private void Update()
+    {
+        CheckInput();
+        CheckMovementDirection();
+    }
+    private void FixedUpdate()
+    {
+        ApplyMovement();
+    }
+    private void CheckMovementDirection()
+    {
+        if(isFacingRight && movementInputDirection < 0)
+        {
+            Flip();
+        }
+        else if(!isFacingRight && movementInputDirection > 0) 
+        {
+            Flip();
+        }
+    }
+    private void CheckInput()
+    {
+        movementInputDirection = Input.GetAxisRaw("Horizontal");
         
+        if(Input.GetButtonDown("Jump"))
+        {
+            Jump();
+        }
+    }
+    private void Jump()
+    {
+        rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+    }
+    private void ApplyMovement()
+    {
+        rb.velocity = new Vector2(movementSpeed * movementInputDirection, rb.velocity.y);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Flip()
     {
-        
+        isFacingRight = !isFacingRight;
+        transform.Rotate(0.0f, 180.0f, 0.0f);
     }
 }
