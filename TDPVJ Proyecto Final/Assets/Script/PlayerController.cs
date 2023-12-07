4360b835648d70ad3954f788fdd44049a112de36
Joaquín Copello <88283010+Joaqqinn@users.noreply.github.com>
2023-12-07 16:48:34 -0300

    cinemachine, jump and animations

--- TDPVJ Proyecto Final/Assets/Script/PlayerController.cs
+++ TDPVJ Proyecto Final/Assets/Script/PlayerController.cs
@@ -6,24 +6,50 @@ public class PlayerController : MonoBehaviour
 {
     //Player
     private Rigidbody2D rb;
+    private Animator anim;
     private float movementInputDirection;
     private bool isFacingRight = true;
+    public bool isWalking;
+    public bool isGrounded;
+    public bool canJump;
 
-    public float movementSpeed = 10.0f;
+    public float movementSpeed = 5.0f;
     public float jumpForce = 16.0f;
+    public float groundCheckRadius;
+    public Transform groundCheck;
+    public LayerMask whatIsGround;
 
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
     }
     private void Update()
     {
         CheckInput();
         CheckMovementDirection();
+        UpdateAnimations();
+        CheckIfCanJump();
     }
     private void FixedUpdate()
     {
         ApplyMovement();
+        CheckSurroundings();
+    }
+    private void CheckSurroundings()
+    {
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, whatIsGround);
+    }
+    private void CheckIfCanJump()
+    {
+        if(isGrounded && rb.velocity.y <= 0.01f)
+        {
+            canJump = true;
+        }
+        else
+        {
+            canJump = false;
+        }
     }
     private void CheckMovementDirection()
     {
@@ -35,6 +61,21 @@ public class PlayerController : MonoBehaviour
         {
             Flip();
         }
+
+        if(rb.velocity.x != 0)
+        {
+            isWalking = true;
+        }
+        else
+        {
+            isWalking = false;
+        }
+    }
+    private void UpdateAnimations()
+    {
+        anim.SetBool("IsWalking", isWalking);
+        anim.SetBool("IsGrounded", isGrounded);
+        anim.SetFloat("yVelocity", rb.velocity.y);
     }
     private void CheckInput()
     {
@@ -47,7 +88,10 @@ public class PlayerController : MonoBehaviour
     }
     private void Jump()
     {
-        rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+        if(canJump)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+        }
     }
     private void ApplyMovement()
     {
@@ -58,5 +102,10 @@ public class PlayerController : MonoBehaviour
     {
         isFacingRight = !isFacingRight;
         transform.Rotate(0.0f, 180.0f, 0.0f);
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawWireSphere(groundCheck.position, groundCheckRadius);        
     }
 }
